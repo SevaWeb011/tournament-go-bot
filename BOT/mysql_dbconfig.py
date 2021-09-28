99a47419eef92e9b231185e2740d2b8c1fc68947
ViktoriaFoil <viktoriazubova449@gmail.com>
2021-09-28 16:32:50 +0300

    fix

--- BOT/mysql_dbconfig.py
+++ BOT/mysql_dbconfig.py
@@ -2,7 +2,7 @@ import os
 
 def read_db_config():
     db = {
-        #"host" : os.getenv("HOST"),
+        "host" : os.getenv("HOST"),
         "database" : os.getenv("DATABASE"),
         "user" : os.getenv("USER"),
         "password" : os.getenv("PASSWORD")
