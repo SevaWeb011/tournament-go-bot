4d9728a2bf99b417ed0cf5bcff20931e6c10ff67
ViktoriaFoil <viktoriazubova449@gmail.com>
2021-10-05 14:45:14 +0300

    update

--- backup.py
+++ backup.py
@@ -1,10 +1,8 @@
 import os
 import time
 
-while True:
-    def backup():
-        try:
-            os.system("mysqldump -u root tournament_go > /tmp/tournament_go.sql")
-        except Exception as e:
-            print(e) 
-    time.sleep(300) # 5мин
+def backup():
+    try:
+        os.system("mysqldump -u root tournament_go > /tmp/tournament_go.sql")
+    except Exception as e:
+        print(e) 
